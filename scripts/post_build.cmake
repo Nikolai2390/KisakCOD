85be33cd02fcc92756efb940e751dbe7c6af4991
Nikolai2390 <75572007+Nikolai2390@users.noreply.github.com>
2026-07-31 23:52:01 +0300

    Update

--- scripts/post_build.cmake
+++ scripts/post_build.cmake
@@ -3,7 +3,7 @@ add_custom_command(
         TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_directory
         ${DEPS_DIR}/msslib/dlls
-        ${BIN_DIR}/$<CONFIG>
+        ${BIN_DIR}/${CMAKE_BUILD_TYPE}
         COMMENT "COPYING MILES DEPENDENCIES"
 )
 # [POST_BUILD] Copy over steam depdendency
@@ -11,6 +11,6 @@ add_custom_command(
         TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
         ${DEPS_DIR}/steamsdk/steam_api.dll
-        ${BIN_DIR}/$<CONFIG>
+        ${BIN_DIR}/${CMAKE_BUILD_TYPE}
         COMMENT "COPYING STEAM DEPENDENCIES"
 )
